5356640b24fa65289763dfcc05abc8fafa27f082
Henrik Joreteg <hjoreteg@gmail.com>
2018-01-26 22:40:54 -0800

    added build command

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ const Layout = ({doUpdateUrl, currentComponent, pathname}) => {
     <NavHelper onInternalNav={doUpdateUrl}>
       <main class='ph3 ph4-ns pt3 bt b--black-10 black-60'>
         <nav class='pa3 pa4-ns'>
-          <p class='b f3 tc f2-ns black-70 lh-solid mb0'>redux-bundler sample app</p>
+          <p class='b f3 tc f2-ns black-70 lh-solid mb0'>redux-bundler sample web-worker app</p>
           <p class='f6 db b tc pb2'>By: <a href='https://twitter.com/henrikjoreteg' class='link blue dim'>@HenrikJoreteg</a></p>
           <div class='tc pb3'>
             {navItems.map(item => {
